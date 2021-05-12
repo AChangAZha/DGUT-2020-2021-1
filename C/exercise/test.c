b67dcc4b7a3e4f5db6e879245188907a598a61f3
ZhangHan <202041404130@dgut.edu.cn>
2021-05-12 23:06:30 +0800

    change

--- C/exercise/test.c
+++ C/exercise/test.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-#define MAX abc
+#include <string.h>
+void enterStr(char *str,int num)
+{
+    char tmp[50];
+    while(1)
+    {
+        scanf("%s",tmp);
+        if(strlen(tmp)<num) break;
+        printf("字数超过限制，请重新输入：");
+        while (getchar()!='\n');
+    }
+    strcpy(str,tmp);
+}
 int main()
 {
-    char a[100];
-    a=MAX;
-    printf("%s",a);
-    return 0;
+    char str[2];
+    enterStr(str,sizeof(str));
 }
